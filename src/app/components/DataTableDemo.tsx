fbd26ff7ae4a6cc131ccd1505a2bb2002e98368f
KimV <71965590+dezineskv@users.noreply.github.com>
2025-07-13 19:05:44 -0700

    addition of Stock value to the data table, sortable values

--- src/app/components/DataTableDemo.tsx
+++ src/app/components/DataTableDemo.tsx
@@ -42,78 +42,91 @@ const data: Payment[] = [
     amount: 316,
     quantity: "bulk",
     products: "latte",
+    stock: "In Stock",
   },
   {
     id: "3u1reuv4",
     amount: 242,
     quantity: "bulk",
     products: "double espresso",
+    stock: "In Stock",
   },
   {
     id: "derv1ws0",
     amount: 837,
     quantity: "1",
     products: "cappucino",
+    stock: "In Stock",
   },
   {
     id: "5kma53ae",
     amount: 874,
     quantity: "bulk",
     products: "espresso",
+    stock: "In Stock",
   },
   {
     id: "bhqecj4p",
     amount: 721,
     quantity: "2",
     products: "coffee beans",
+    stock: "Out of Stock",
   },
   {
     id: "bhqecj4p",
     amount: 123,
     quantity: "bulk",
     products: "coffee",
+    stock: "In Stock",
   },
   {
     id: "gbhqecj4p",
     amount: 500,
     quantity: "3",
     products: "coffee cake",
+    stock: "Out of Stock",
   },
   {
     id: "bhqecj4pj",
     amount: 250,
     quantity: "bulk",
     products: "breakfast sandwich",
+    stock: "In Stock",
   },
   {
     id: "gbhq5ecj4p",
     amount: 500,
     quantity: "bulk",
     products: "donut",
+    stock: "In Stock",
   },
   {
     id: "bhq7ecj4pj",
     amount: 250,
     quantity: "bulk",
     products: "drip coffee",
+    stock: "In Stock",
   },
   {
     id: "bhqecj49pj",
     amount: 250,
     quantity: "bulk",
     products: "iced coffee",
+    stock: "In Stock",
   },
   {
     id: "b0hqecj49pj",
     amount: 250,
     quantity: "bulk",
     products: "americano",
+    stock: "In Stock",
   },
   {
     id: "b8hqecj49pj",
     amount: 250,
     quantity: "bulk",
     products: "tea",
+    stock: "In Stock",
   },
 ];
 
@@ -122,6 +135,7 @@ export type Payment = {
   amount: number;
   quantity: "1" | "2" | "3" | "bulk";
   products: string;
+  stock: string;
 };
 
 export const columns: ColumnDef<Payment>[] = [
@@ -170,6 +184,24 @@ export const columns: ColumnDef<Payment>[] = [
     },
     cell: ({ row }) => (
       <div className="lowercase pl-4">{row.getValue("products")}</div>
+    ),
+  },
+  {
+    accessorKey: "stock",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          className="font-extrabold"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Stock
+          <ArrowUpDown />
+        </Button>
+      );
+    },
+    cell: ({ row }) => (
+      <div className="lowercase pl-4">{row.getValue("stock")}</div>
     ),
   },
   {
@@ -361,7 +393,7 @@ export default function DataTableDemo() {
               Next
             </Button>
           </div>
-          <Button className="bg-gray-900 text-white sm:mx-6 py-6 md:px-5 rounded-md text-md shadow-lg hover:scale-105 transition-all w-1/2">
+          <Button className="bg-gray-900 text-white sm:mx-6 py-6 md:px-5 rounded-md text-md shadow-lg hover:scale-105 transition-all max-w-1/2">
             Order Now
           </Button>
         </div>
