dd1c81f805b109327752fc56d8983259fab11a95
KimV <71965590+dezineskv@users.noreply.github.com>
2025-07-29 15:50:20 -0700

    inventory page temp update

--- src/app/(mainLayout)/inventory/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import SingleInv from "../../../components/SingleInv";
-
-const ProductId = async ({ params }: { params: Promise<{ id: string }> }) => {
-  const id = (await params).id;
-
-  return <SingleInv id={id} />;
-};
-
-export default ProductId;
