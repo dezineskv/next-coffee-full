5da0a76de2bfefcfccdd53751b095e860041caee
KimV <71965590+dezineskv@users.noreply.github.com>
2025-08-13 23:10:19 -0700

    adjustment to admin form css

--- src/app/components/Forms.tsx
+++ src/app/components/Forms.tsx
@@ -18,12 +18,13 @@ export default function Forms() {
             ref.current?.reset();
             await createProducts(FormData);
           }}
-          className="flex flex-col justify-center mx-auto border-1 border-black rounded-lg shadow-lg p-6 mt-8 w-3xl"
+          className="flex flex-col justify-center mx-auto border-1 border-black rounded-lg shadow-lg p-6 gap-4 mt-8 w-[85%] md:w-3xl"
         >
           {/* <label htmlFor="product" className="py-2">
         Product
       </label>
       <input type="text" name="product" className="mb-2  w-62 h-10 p-2 rounded-lg border-1 border-black" /> */}
+          <div>
           <label htmlFor="title" className="py-2 pl-5 text-left text-black">
             Title
           </label>
@@ -32,6 +33,8 @@ export default function Forms() {
             name="title"
             className="w-62 h-10 p-2 ml-2 rounded-lg border-1 border-black text-black"
           />
+          </div>
+          <div>
           <label
             htmlFor="description"
             className="py-2 pl-5 mt-2 text-left text-black"
@@ -42,7 +45,8 @@ export default function Forms() {
             type="text"
             name="description"
             className="w-82 h-10 p-2 ml-2 rounded-lg border-1 border-black text-black"
-          />
+          /></div>
+          <div>
           <label
             htmlFor="price"
             className="py-2 pl-5 mt-2 text-left text-black"
@@ -52,17 +56,29 @@ export default function Forms() {
           <input
             type="string"
             name="price"
-            className="w-42 h-10 p-2 ml-2 rounded-lg border-1 border-black text-black"
+            className="w-32 h-10 p-2 ml-2 rounded-lg border-1 border-black text-black"
             defaultValue="5"
           />
-          <div className="flex flex-col mx-auto">
+          </div>
+          <div className="flex flex-col mx-auto text-left p-6 bg-gray-300">
             <h3 className="text-blue-500 text-bold">Defaults</h3>
-          <input name="image_url" type="string" defaultValue="https://i.ibb.co/7xJPr5xq/coffee-6.jpg" className="text-black"/>
-          <input name="roast_level" type="string" defaultValue="6"  className="text-black"/>
-           <input name="origin" type="string" defaultValue="1" className="text-black"/>
-          <input name="in_stock" type="string" defaultValue="yes" className="text-black"/>
-          <input name="weight_oz" type="string" defaultValue="12" className="text-black"/>              
+            <p>
+            Image:&nbsp;
+          <input name="image_url" type="string" defaultValue="https://i.ibb.co/7xJPr5xq/coffee-6.jpg" className="text-black"/></p>
+          <p>
+          Roast level:&nbsp;
+          <input name="roast_level" type="string" defaultValue="6"  className="text-black"/></p>
+          <p>
+          Origin:&nbsp;
+           <input name="origin" type="string" defaultValue="Columbia" className="text-black"/></p>
+           <p>
+           In stock:&nbsp; <input name="in_stock" type="string" defaultValue="yes" className="text-black"/></p>
+          <p>Weight oz.:&nbsp;
+          <input name="weight_oz" type="string" defaultValue="12" className="text-black"/></p>            
+          <p>  
+          Sale:&nbsp;
           <input name="sale" type="string" defaultValue="no" className="text-black"/>
+          </p>
           </div>
           <SubmitButton />
         </form>
--- src/app/components/SubmitButton.tsx
+++ src/app/components/SubmitButton.tsx
@@ -7,7 +7,7 @@ export default function SubmitButton() {
     <button
       type="submit"
       {...(pending && { disabled: true })}
-      className={`bg-blue-400 h-10 w-62 p-2 mt-10  rounded text-white font-bold text-center mx-auto ${
+      className={`bg-blue-400 h-10 w-62 p-2 mt-2 rounded text-white font-bold text-center mx-auto ${
         pending ? "cursor-not-allowed" : "cursor-pointer"
       }`}
     >
