3f442b7fe9b4ecff8e7450f739c9114f239fdc01
KimV <71965590+dezineskv@users.noreply.github.com>
2025-08-23 20:42:33 -0700

    testing, update to tsconfig

--- src/app/(mainLayout)/products/[id]/page.tsx
+++ src/app/(mainLayout)/products/[id]/page.tsx
@@ -23,7 +23,7 @@ type Params = {
   };
 };
 
-export default async function ProductPage({ params }: Params) {
+export default async function ProductPage({ params }: { params: { id: string } }) {
   const { id } = params;
   const product = await getProductById(id);
 
