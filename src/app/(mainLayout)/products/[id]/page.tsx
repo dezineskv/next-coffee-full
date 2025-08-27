5c4dcef47d3cc32f30af088aa048b71964375240
KimV <71965590+dezineskv@users.noreply.github.com>
2025-08-26 22:52:16 -0700

    update to id page

--- src/app/(mainLayout)/products/[id]/page.tsx
+++ src/app/(mainLayout)/products/[id]/page.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import DetailsPage from './_components/DetailsPage';
 
-const page = async ({ id }: { id: string }) => {
-  // const page = async ({ params }: { params: { id: string } }) => {
-  // const { id } = await params;
-  // const { id } = await id;
-  return (
-    <>
-      <DetailsPage id={id} />
-    </>
-  );
+const page = async ({ params }: { params: { id: string } }) => {
+  const { id } = params; // ✅ No need to await
+  return <DetailsPage id={id} />;
 };
+// const page = async ({ params }: { params: { id: string } }) => {
 
 export default page;
