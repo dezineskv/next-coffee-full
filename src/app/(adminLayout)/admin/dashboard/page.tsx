8823a23ae17ce59418eacc265b89803521fd01fe
KimV <71965590+dezineskv@users.noreply.github.com>
2025-08-26 23:36:51 -0700

    update to admin chart

--- src/app/(adminLayout)/admin/dashboard/page.tsx
+++ src/app/(adminLayout)/admin/dashboard/page.tsx
@@ -2,15 +2,15 @@ import { Chartbar } from '@/app/components/Chartbar';
 import { Chartsmall } from '@/app/components/Chartsmall';
 import { Chartline } from '@/app/components/Chartline';
 import Chartpie from '@/app/components/Chartpie';
-import { ChartbarTwo } from '@/app/components/ChartbarTWo';
+// import { ChartbarTwo } from '@/app/components/ChartbarTWo';
 
 import React from 'react';
 
 function page() {
   return (
     <>
-      <div className="w-screen bg-slate-200 md:pl-20 flex flex-col gap-4">
-        <h1 className="text-left mt-20 mb-10 text-2xl font-bold">Admin Dashboard</h1>
+      <div className="flex w-screen flex-col gap-4 bg-slate-200 md:pl-20">
+        <h1 className="mt-20 mb-10 text-left text-2xl font-bold">Admin Dashboard</h1>
         <div className="row mt-10 flex w-2xl justify-between">
           <Chartline /> <Chartpie />
         </div>
@@ -18,7 +18,8 @@ function page() {
           <Chartbar />
         </div>
         <div className="row my-10 flex w-2xl justify-between">
-          <Chartsmall /> <ChartbarTwo />
+          <Chartsmall />
+          {/* <ChartbarTwo /> */}
         </div>
       </div>
     </>
